e9b9da0f21cb804a6ab9d1f959aa6988c162bfe9
Carlos Cordero <carlos.cordero@odigeo.com>
2017-07-12 08:52:39 +0200

    added pet contract URI

--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -3,12 +3,14 @@ package com.example.android.pets.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
+import android.net.Uri;
 
 public class PetDbHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION=1;
     public static final String DATABASE_NAME="shelter.db";
+
+
 
     public PetDbHelper(Context context){
         super(context, DATABASE_NAME,null, DATABASE_VERSION);
